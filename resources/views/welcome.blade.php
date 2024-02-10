af34f520e09b653d57dff855199ebe88cd979974
Omarvl21 <omarvl21@outlook.com>
2024-02-10 17:58:45 -0600

    cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
 				          <li>                           <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar</a>
 </li>				          <li>                           <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registrar</a>
 
-				         <li><a href="otros.html">RECOMENDACIONES</a></li>
+				         <li><a href="otros.html">Hola </a></li>
 				          <!--<li><a href="dogs.html">Revistas</a></li>
 				          <li><a href="volunteer.html">Lectores</a></li>
 				          <li class="menu-has-children"><a href="">Blog</a>
